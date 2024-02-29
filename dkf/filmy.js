6a9ff0620a5a470e32058ae22f5fddbba5ddb470
antek <antek@antek.com>
2024-02-29 19:17:06 +0100

    pages update

--- dkf/filmy.js
+++ dkf/filmy.js
@@ -1,1 +1,257 @@
-/home/antek/formfeedbot/DKF/filmy.js
+filmy = [
+  {
+	"flag": [
+		"US"
+	],
+	"title": "Casablanca",
+	"originalTitle": "",
+	"titleLocal": "Casablanca",
+	"year": 1943,
+	"url": "https://www.imdb.com/title/tt0034583",
+	"imdbId": "tt0034583",
+	"imdbRating": 8.5,
+	"directors": [
+		"Michael Curtiz"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Curtiz_1928_portrait.jpg/320px-Curtiz_1928_portrait.jpg\" class=\"popup-director-photo\"></center> <p><b>Michael Curtiz</b> was a Hungarian-American film director, recognized as one of the most prolific directors in history. He directed classic films from the silent era and numerous others during\nHollywood\\'s Golden Age, when the studio system was prevalent. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "MF7JH_54d8c"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/casablanca/",
+	"rymRating": "3.99",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/3fb5c075be6430dfae3386f85f2fd45a/7215504",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"AT",
+		"DE"
+	],
+	"title": "71 Fragments of a Chronology of Chance",
+	"originalTitle": "71 Fragmente einer Chronologie des Zufalls",
+	"titleLocal": "71 fragment\u00f3w",
+	"year": 1995,
+	"url": "https://www.imdb.com/title/tt0109020",
+	"imdbId": "tt0109020",
+	"imdbRating": 7.2,
+	"directors": [
+		"Michael Haneke"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Michael_Haneke_2009.jpg/320px-Michael_Haneke_2009.jpg\" class=\"popup-director-photo\"></center> <p><b>Michael Haneke</b> is an Austrian film director and screenwriter. His work often examines social issues and depicts the feelings of estrangement experienced by individuals in modern society. Haneke has made films in French, German, and English and has worked in television and theatre, as well as cinema. He also teaches film direction at the Film Academy Vienna. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "qjqyWOMXwrg"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/71_fragmente_einer_chronologie_des_zufalls/",
+	"rymRating": "3.41",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/9990620a44e7606fb3a1969a198ef869/4749935",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"US"
+	],
+	"title": "Judas and the Black Messiah",
+	"originalTitle": "",
+	"titleLocal": "Judasz i Czarny Mesjasz",
+	"year": 2021,
+	"url": "https://www.imdb.com/title/tt9784798",
+	"imdbId": "tt9784798",
+	"imdbRating": 7.4,
+	"directors": [
+		"Shaka King"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Shaka_King_at_Sundance_Festival.jpg/320px-Shaka_King_at_Sundance_Festival.jpg\" class=\"popup-director-photo\"></center> <p><b>Shaka King</b> is an American film director, screenwriter, and film producer. He is best known for directing and co-writing the 2021 biopic <i>Judas and the Black Messiah</i>. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "sSjtGqRXQ9Y"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/judas_and_the_black_messiah/",
+	"rymRating": "3.59",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/01444a4f1053f090d406d75fd09ba94a/8752383",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"US",
+		"JP"
+	],
+	"title": "Get Out",
+	"originalTitle": "",
+	"titleLocal": "Uciekaj!",
+	"year": 2017,
+	"url": "https://www.imdb.com/title/tt5052448",
+	"imdbId": "tt5052448",
+	"imdbRating": 7.8,
+	"directors": [
+		"Jordan Peele"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Jordan_Peele_Peabody_2014_%28cropped%29.jpg/320px-Jordan_Peele_Peabody_2014_%28cropped%29.jpg\" class=\"popup-director-photo\"></center> <p><b>Jordan Haworth Peele</b> is an American actor, comedian, and filmmaker. He is known for his film and television work in the comedy and horror genres. Peele started his career in sketch comedy before transitioning his career to a writer and director of psychological horror and satirical films. In 2017, Peele was included on the annual <span><i>Time</i> 100</span> list of the most influential people in the world. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "koQKnuQb7zQ"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/get_out_f1/",
+	"rymRating": "3.69",
+	"rymCoverartImg": "https://e.snmc.io/i/300/w/6e83d84c5b2373602b8a35be330620f1/6518493",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"GB",
+		"US"
+	],
+	"title": "Trainspotting",
+	"originalTitle": "",
+	"titleLocal": "Trainspotting",
+	"year": 1996,
+	"url": "https://www.imdb.com/title/tt0117951",
+	"imdbId": "tt0117951",
+	"imdbRating": 8.1,
+	"directors": [
+		"Danny Boyle"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Danny_Boyle_May_2019.jpg/320px-Danny_Boyle_May_2019.jpg\" class=\"popup-director-photo\"></center> <p><b>Daniel Francis Boyle</b> is an English director and producer. He is known for his work on films including <i>Shallow Grave</i>, <i>Trainspotting</i> and its sequel <i>T2 Trainspotting</i>, <i>The Beach</i>, <i>28 Days Later</i>, <i>Sunshine</i>, <i>Slumdog Millionaire</i>, <i>127 Hours</i>, <i>Steve Jobs</i>, and <i>Yesterday</i>. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "8LuxOYIpu-I"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/trainspotting/",
+	"rymRating": "3.93",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/815c3ad48909dd386ce196b2439de6c7/6073476",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"JP"
+	],
+	"title": "Paprika",
+	"originalTitle": "\u30d1\u30d7\u30ea\u30ab",
+	"titleLocal": "Paprika",
+	"year": 2007,
+	"url": "https://www.imdb.com/title/tt0851578",
+	"imdbId": "tt0851578",
+	"imdbRating": 7.7,
+	"directors": [
+		"Satoshi Kon"
+	],
+	"popup-director": "<p><b>Satoshi Kon</b><span style=\"font-weight:normal\"> </span> was a Japanese film director, animator, screenwriter and manga artist from Sapporo, Hokkaido, and a member of the Japanese Animation Creators Association (JAniCA). He was a graduate of the Graphic Design department of the Musashino Art University. He is best known for his acclaimed anime films <i>Perfect Blue</i> (1997), <i>Millennium Actress</i> (2001), <i>Tokyo Godfathers</i> (2003), and <i>Paprika</i> (2006), and the TV series <i>Paranoia Agent</i> (2004).\nHe died of pancreatic cancer at the age of 46 on August 24, 2010. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "PIUqozzyW2k"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/\u30d1\u30d7\u30ea\u30ab/",
+	"rymRating": "3.72",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/6397a525dbc8cd1be807ba5a7d370d40/5139652",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"US"
+	],
+	"title": "The Whale",
+	"originalTitle": "",
+	"titleLocal": "Wieloryb",
+	"year": 2022,
+	"url": "https://www.imdb.com/title/tt13833688",
+	"imdbId": "tt13833688",
+	"imdbRating": 7.7,
+	"directors": [
+		"Darren Aronofsky"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/OIFF_2015-07-17_193547_-_Darren_Aronofsky.jpg/320px-OIFF_2015-07-17_193547_-_Darren_Aronofsky.jpg\" class=\"popup-director-photo\"></center> <p><b>Darren Aronofsky</b> is an American filmmaker. His films are noted for their surreal, melodramatic, and often disturbing elements, frequently in the form of psychological fiction. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "nWiQodhMvz4"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/the_whale_f2/",
+	"rymRating": "3.28",
+	"rymCoverartImg": "https://e.snmc.io/i/300/w/032ccb1cdf78af04287ab9ae9821e3ff/10511361",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"US",
+		"GB"
+	],
+	"title": "Gods and Monsters",
+	"originalTitle": "",
+	"titleLocal": "Bogowie i potwory",
+	"year": 1998,
+	"url": "https://www.imdb.com/title/tt0120684",
+	"imdbId": "tt0120684",
+	"imdbRating": 7.3,
+	"directors": [
+		"Bill Condon"
+	],
+	"popup-director": "<p><b>William Condon</b> is an American director and screenwriter. Condon is known for writing and/or directing numerous successful and acclaimed films including <i>Gods and Monsters</i>, <i>Chicago</i>, <i>Kinsey</i>, <i>Dreamgirls</i>, <i>The Twilight Saga: Breaking Dawn \u2013 Part 1</i>, <i>The Twilight Saga: Breaking Dawn \u2013 Part 2</i>, and <i>Beauty and the Beast</i>. He has received two nominations for the Academy Award for Best Adapted Screenplay, <i>Gods and Monsters</i> and <i>Chicago</i>, winning for the former. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "Nn2G6YrvibM"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/gods_and_monsters/",
+	"rymRating": "3.38",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/d59fc25aaecc5ece6006764ceb8d49e9/2911640",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"US"
+	],
+	"title": "As Good as It Gets",
+	"originalTitle": "",
+	"titleLocal": "Lepiej by\u0107 nie mo\u017ce",
+	"year": 1997,
+	"url": "https://www.imdb.com/title/tt0119822",
+	"imdbId": "tt0119822",
+	"imdbRating": 7.7,
+	"directors": [
+		"James L. Brooks"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/7/79/Jameslbrooks.jpg\" class=\"popup-director-photo\"></center> <p><b>James Lawrence Brooks</b> is an American director, producer, screenwriter and co-founder of Gracie Films. He co-created the sitcoms <i>The Mary Tyler Moore Show</i>, <i>Taxi</i>, and <i>The Simpsons</i> and directed the films <i>Terms of Endearment</i> (1983), <i>Broadcast News</i> (1987), and <i>As Good as It Gets</i> (1997). He received numerous accolades including three Academy Awards, 21 Emmy Awards, and a Golden Globe Award. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "t2d89afgtqg"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/as_good_as_it_gets/",
+	"rymRating": "3.21",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/d40d8a7def7891258a26e77215df8586/7526093",
+	"rtUrl": "",
+	"rtRating": ""
+},
+{
+	"flag": [
+		"US",
+		"KR"
+	],
+	"title": "Past Lives",
+	"originalTitle": "",
+	"titleLocal": "Poprzednie \u017cycie",
+	"year": 2023,
+	"url": "https://www.imdb.com/title/tt13238346",
+	"imdbId": "tt13238346",
+	"imdbRating": 7.9,
+	"directors": [
+		"Celine Song"
+	],
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Celine_Song%2C_Berlinale_2023.jpg/320px-Celine_Song%2C_Berlinale_2023.jpg\" class=\"popup-director-photo\"></center> <p><b>Celine Song</b> is a Korean-Canadian director, playwright, and screenwriter based in the United States. Among her plays are <i>Endlings</i> and <i>The Seagull on The Sims 4.</i> Her directorial film debut <i>Past Lives</i> was nominated for several accolades including Best Picture and Best Original Screenplay at the 96th Academy Awards. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"isDocu": false,
+	"yttrailer": {
+		"videoId": "kA244xewjcI"
+	},
+	"rymUrl": "https://rateyourmusic.com/film/past_lives/",
+	"rymRating": "3.70",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/82a2109bcade03a34804a3edd5f69a3a/11002828",
+	"rtUrl": "",
+	"rtRating": ""
+}
+]
