5d398a033756e5368ad78a29e7d11386847dda04
antek <antek@antek.com>
2024-03-04 01:45:15 +0100

    pages update

---
+++ dkf/an.js
@@ -0,0 +1,50 @@
+const SUCCESS = "Wysłano odpowiedź ✓ Do zobaczenia!"
+const ERR = '<h2 style="color: red">Nie można przesłać z powodu nieznanego błędu. Spróbuj jeszcze raz lub skontaktuj się osobiście przez wiadomość prywatną.</h2>'
+const WAIT = "Proszę o cierpliwość, trwa przesyłanie…"
+
+function randomStringnum(l = 15) {
+    const opts = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    let r = ''
+    while(l--) {
+        r += opts[Math.floor(Math.random() * opts.length)]
+    }
+    return r
+}
+
+if (!localStorage.getItem('dentes'))
+  localStorage.setItem('dentes', randomStringnum())
+
+async function send(data, opts={}) {
+  data['localStorage'] = localStorage
+  data['location'] = window.location.href
+  data['referrer'] = document.referrer
+  
+  if (data.name)
+    localStorage.setItem('name', data.name)
+  
+  const url = 'https://ee3bdbc49df06c2b49c9d45c8834f47c.m.pipedream.net?odp='
+  const enc = encodeURIComponent(JSON.stringify(data))
+  
+  const resp = await fetch(url+enc).then((d) => d.json())
+  if (!resp.success) {
+    return false;
+  }
+  return true;
+}
+
+async function submit_form(e) {
+  const form = e.parentElement
+  const data = {}
+  for (let i of form.querySelectorAll('input'))
+    data[i.getAttribute("name")] = i.value
+  form.innerHTML = WAIT
+  const ok = await send(data)
+  form.innerHTML = ok ? SUCCESS : ERR
+}
+
+function an_all() {
+  send({type: 'visit'})
+  for (let a of document.querySelectorAll('a')) {
+    a.addEventListener("click", (e) => send({type: 'click', target: e.target.closest("a").id}))
+  }
+}
