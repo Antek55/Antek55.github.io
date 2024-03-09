cd45aa467bcfb9793f200e5dff8757d25b2dc68a
antek <antek@antek.com>
2024-03-09 15:15:09 +0100

    pages update

--- dkf/filmy.js
+++ dkf/filmy.js
@@ -1,316 +1,79 @@
 filmy = [
   {
-  	"flag": [
-  		"US"
-  	],
-  	"title": "American Fiction",
-  	"originalTitle": "",
-  	"titleLocal": "Ameryka\u0144ska fikcja",
-  	"year": 2023,
-  	"url": "https://www.imdb.com/title/tt23561236",
-  	"imdbId": "tt23561236",
-  	"imdbRating": 7.6,
-  	"directors": [
-  		"Cord Jefferson"
-  	],
-  	"popup-director": "<p><b>Cord Jefferson</b> is an American writer and director. After studying at the College of William &amp; Mary he started a career in journalism where he wrote for numerous publications before becoming an editor at <i>Gawker</i> until 2016. <i class=\"fab fa-wikipedia-w\"></i></p>",
-  	"isDocu": false,
-  	"yttrailer": {
-  		"videoId": "i0MbLCpYJPA"
-  	},
-  	"rymUrl": "https://rateyourmusic.com/film/american_fiction/",
-  	"rymRating": "3.39",
-  	"rymCoverartImg": "https://e.snmc.io/i/300/s/4953d089124d9453f57d1a92389a0006/11427309",
-  	"rtUrl": "",
-  	"rtRating": ""
-  },
-  {
 	"flag": [
-		"FR"
+		"JP"
 	],
-	"title": "Anatomy of a Fall",
-	"originalTitle": "Anatomie d'une chute",
-	"titleLocal": "Anatomia upadku",
-	"year": 2024,
-	"url": "https://www.imdb.com/title/tt17009710",
-	"imdbId": "tt17009710",
-	"imdbRating": 7.8,
+	"title": "Throne of Blood",
+	"originalTitle": "\u8718\u86db\u5de3\u57ce",
+	"titleLocal": "Tron we krwi",
+	"year": 1961,
+	"url": "https://www.imdb.com/title/tt0050613",
+	"imdbId": "tt0050613",
+	"imdbRating": 8.1,
 	"directors": [
-		"Justine Triet"
+		"Akira Kurosawa"
 	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Justine_Triet_2017.jpg/320px-Justine_Triet_2017.jpg\" class=\"popup-director-photo\"></center> <p><b>Justine Triet</b> is a French film director, screenwriter and editor. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Akirakurosawa-onthesetof7samurai-1953-page88.jpg/320px-Akirakurosawa-onthesetof7samurai-1953-page88.jpg\" class=\"popup-director-photo\"></center> <p><b>Akira Kurosawa</b><span style=\"font-weight:normal\"> </span> was a Japanese filmmaker and painter who directed 30 films in a career spanning over five decades. He is widely regarded as one of the greatest and most influential filmmakers in the history of cinema. Kurosawa displayed a bold, dynamic style, strongly influenced by Western cinema yet distinct from it; he was involved with all aspects of film production. <i class=\"fab fa-wikipedia-w\"></i></p>",
 	"isDocu": false,
 	"yttrailer": {
-		"videoId": "FUXawkH-ONM"
+		"videoId": "LY7ETIO1KB8"
 	},
-	"rymUrl": "https://rateyourmusic.com/film/anatomie_dune_chute/",
-	"rymRating": "3.82",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/6e1024589d7c97dc63dd2fd17622b702/11062748",
+	"rymUrl": "https://rateyourmusic.com/film/\u8718\u86db\u5de3\u57ce/",
+	"rymRating": "3.97",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/3daaf3e58c8ce36d18021d9a96e0e58f/5098717",
 	"rtUrl": "",
 	"rtRating": ""
 },
 {
 	"flag": [
-		"US"
-	],
-	"title": "Killers of the Flower Moon",
-	"originalTitle": "",
-	"titleLocal": "Czas krwawego ksi\u0119\u017cyca",
-	"year": 2023,
-	"url": "https://www.imdb.com/title/tt5537002",
-	"imdbId": "tt5537002",
-	"imdbRating": 7.7,
-	"directors": [
-		"Martin Scorsese"
-	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Martin_Scorsese_at_Berlinale_2024-6_Ausschnitt.jpg/320px-Martin_Scorsese_at_Berlinale_2024-6_Ausschnitt.jpg\" class=\"popup-director-photo\"></center> <p><b>Martin Charles Scorsese</b> is an American filmmaker. He emerged as one of the major figures of the New Hollywood era. Scorsese has received many accolades, including an Academy Award, four BAFTA Awards, three Emmy Awards, a Grammy Award, three Golden Globe Awards, and two Directors Guild of America Awards. He has been honored with the AFI Life Achievement Award in 1997, the Film Society of Lincoln Center tribute in 1998, the Kennedy Center Honor in 2007, the Cecil B. DeMille Award in 2010, and the BAFTA Fellowship in 2012. Five of his films have been inducted into the National Film Registry by the Library of Congress as \"culturally, historically or aesthetically significant\". <i class=\"fab fa-wikipedia-w\"></i></p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "EP34Yoxs3FQ"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/killers_of_the_flower_moon/",
-	"rymRating": "3.86",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/6e5c818760be72fd20fd8c0b9db64901/11418372",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"US"
-	],
-	"title": "Maestro",
-	"originalTitle": "",
-	"titleLocal": "Maestro",
-	"year": 2023,
-	"url": "https://www.imdb.com/title/tt5535276",
-	"imdbId": "tt5535276",
-	"imdbRating": 6.6,
-	"directors": [
-		"Bradley Cooper"
-	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Bradley_Cooper_2023_%28cropped%29.jpg/320px-Bradley_Cooper_2023_%28cropped%29.jpg\" class=\"popup-director-photo\"></center> <p><b>Bradley Charles Cooper</b> is an American actor and filmmaker. He is the recipient of various accolades, including a British Academy Film Award and two Grammy Awards, in addition to nominations for twelve Academy Awards, six Golden Globe Awards, and a Tony Award. Cooper appeared on the <span><i>Forbes</i> Celebrity 100</span> three times and on <i>Time</i><span class=\"nowrap\">\\'</span>s list of the 100 most influential people in the world in 2015. His films have grossed $13<span class=\"nowrap\">\u00a0</span>billion worldwide and he has placed four times in annual rankings of the world\\'s highest-paid actors. <i class=\"fab fa-wikipedia-w\"></i></p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "gJP2QblqLA0"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/maestro_f4/",
-	"rymRating": "2.70",
-	"rymCoverartImg": "https://e.snmc.io/i/300/w/52617b0c0f1904b911d1c0b5cddcbafa/11489597",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"US",
-		"KR"
-	],
-	"title": "Past Lives",
-	"originalTitle": "",
-	"titleLocal": "Poprzednie \u017cycie",
-	"year": 2023,
-	"url": "https://www.imdb.com/title/tt13238346",
-	"imdbId": "tt13238346",
-	"imdbRating": 7.9,
-	"directors": [
-		"Celine Song"
-	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Celine_Song%2C_Berlinale_2023.jpg/320px-Celine_Song%2C_Berlinale_2023.jpg\" class=\"popup-director-photo\"></center> <p><b>Celine Song</b> is a Korean-Canadian director, playwright, and screenwriter based in the United States. Among her plays are <i>Endlings</i> and <i>The Seagull on The Sims 4.</i> Her directorial film debut <i>Past Lives</i> was nominated for several accolades including Best Picture and Best Original Screenplay at the 96th Academy Awards. <i class=\"fab fa-wikipedia-w\"></i></p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "kA244xewjcI"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/past_lives/",
-	"rymRating": "3.70",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/82a2109bcade03a34804a3edd5f69a3a/11002828",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"IE",
-		"GB",
-		"US",
-		"HU"
-	],
-	"title": "Poor Things",
-	"originalTitle": "",
-	"titleLocal": "Biedne istoty",
-	"year": 2024,
-	"url": "https://www.imdb.com/title/tt14230458",
-	"imdbId": "tt14230458",
-	"imdbRating": 8.3,
-	"directors": [
-		"Yorgos Lanthimos"
-	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Yorgos_Lanthimos_2023_%28cropped%29.jpg/320px-Yorgos_Lanthimos_2023_%28cropped%29.jpg\" class=\"popup-director-photo\"></center> <p><b>Georgios</b> \"<b>Yorgos</b>\" <b>Lanthimos</b> is a Greek filmmaker. He has received numerous accolades, including a BAFTA Film Award and a Golden Globe Award as well as nominations for six Academy Awards. <i class=\"fab fa-wikipedia-w\"></i></p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "S5KiC0GgED0"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/poor_things/",
-	"rymRating": "3.91",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/e2d6f74c77251238bc4f316bf29465b4/11281044",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"US",
-		"GB",
 		"PL"
 	],
-	"title": "The Zone of Interest",
-	"originalTitle": "",
-	"titleLocal": "Strefa interes\u00f3w",
-	"year": 2024,
-	"url": "https://www.imdb.com/title/tt7160372",
-	"imdbId": "tt7160372",
-	"imdbRating": 7.6,
+	"title": "Ashes and Diamonds",
+	"originalTitle": "Popi\u00f3\u0142 i diament",
+	"titleLocal": "Popi\u00f3\u0142 i diament",
+	"year": 1961,
+	"url": "https://www.imdb.com/title/tt0052080",
+	"imdbId": "tt0052080",
+	"imdbRating": 7.7,
 	"directors": [
-		"Jonathan Glazer"
+		"Andrzej Wajda"
 	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/ZoneIntrestBFILFF121023_%287_of_31%29_%2853255110321%29_%28cropped%29_2.jpg/320px-ZoneIntrestBFILFF121023_%287_of_31%29_%2853255110321%29_%28cropped%29_2.jpg\" class=\"popup-director-photo\"></center> <p><b>Jonathan Glazer</b> is an English film director and screenwriter. He began his career in theatre before transitioning into film, directing the features <i>Sexy Beast</i> (2000), <i>Birth</i> (2004), <i>Under the Skin</i> (2013), and <i>The Zone of Interest</i> (2023). <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Andrzej-Wajda-1963.jpg/320px-Andrzej-Wajda-1963.jpg\" class=\"popup-director-photo\"></center> <p><b>Andrzej Witold Wajda</b> was a Polish film and theatre director. Recipient of an Honorary Oscar, the Palme d\\'Or, as well as Honorary Golden Lion and Honorary Golden Bear Awards, he was a prominent member of the \"Polish Film School\". He was known especially for his trilogy of war films consisting of <i>A Generation</i> (1955), <i>Kana\u0142</i> (1957) and <i>Ashes and Diamonds</i> (1958). <i class=\"fab fa-wikipedia-w\"></i></p>",
 	"isDocu": false,
 	"yttrailer": {
-		"videoId": "r-vfg3KkV54"
+		"videoId": "L6CAOVuO-t0"
 	},
-	"rymUrl": "https://rateyourmusic.com/film/the_zone_of_interest/",
-	"rymRating": "3.77",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/4b7f4102b70141ba7c277ff7fbbfe476/11422337",
+	"rymUrl": "https://rateyourmusic.com/film/popiol_i_diament/",
+	"rymRating": "3.88",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/2eff2f9f47f455485d64261c4b4dbda5/4343555",
 	"rtUrl": "",
 	"rtRating": ""
 },
 {
 	"flag": [
-		"US",
-		"GB"
-	],
-	"title": "Oppenheimer",
-	"originalTitle": "",
-	"titleLocal": "Oppenheimer",
-	"year": 2023,
-	"url": "https://www.imdb.com/title/tt15398776",
-	"imdbId": "tt15398776",
-	"imdbRating": 8.4,
-	"directors": [
-		"Christopher Nolan"
-	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Christopher_Nolan_Cannes_2018.jpg/320px-Christopher_Nolan_Cannes_2018.jpg\" class=\"popup-director-photo\"></center> <p><b>Christopher Edward Nolan</b> is a British and American filmmaker. Known for his Hollywood blockbusters with complex storytelling, Nolan is considered a leading filmmaker of the 21st century. He has received many accolades and his films have grossed more than $6\u00a0billion worldwide, ranking him among the highest-grossing directors. In 2015, he was listed as one of the 100 most influential people in the world by <i>Time</i>, and in 2024, he was honoured with the British Film Institute Fellowship. <i class=\"fab fa-wikipedia-w\"></i></p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "uYPbbksJxIg"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/oppenheimer/",
-	"rymRating": "3.70",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/029868d60b041f5fcfaa42e9a26e8885/10512973",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"IT",
-		"BE",
-		"FR"
-	],
-	"title": "Io Capitano",
-	"originalTitle": "Io capitano",
-	"titleLocal": "Ja, kapitan",
-	"year": 2024,
-	"url": "https://www.imdb.com/title/tt14225838",
-	"imdbId": "tt14225838",
-	"imdbRating": 7.6,
-	"directors": [
-		"Matteo Garrone"
-	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Matteo_Garrone-5903.jpg/320px-Matteo_Garrone-5903.jpg\" class=\"popup-director-photo\"></center> <p><b>Matteo Garrone</b> is an Italian filmmaker. <i class=\"fab fa-wikipedia-w\"></i></p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "D0j2Y9e3PxI"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/io_capitano/",
-	"rymRating": "3.34",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/baf37c295de5c88c0318c6676c482cae/11170335",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"JP",
-		"DE"
-	],
-	"title": "Perfect Days",
-	"originalTitle": "Perfect Days",
-	"titleLocal": "Perfect Days",
-	"year": 2023,
-	"url": "https://www.imdb.com/title/tt27503384",
-	"imdbId": "tt27503384",
-	"imdbRating": 7.9,
-	"directors": [
-		"Wim Wenders"
-	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/MJK30764_Wim_Wenders_%28Berlinale_2017%29.jpg/320px-MJK30764_Wim_Wenders_%28Berlinale_2017%29.jpg\" class=\"popup-director-photo\"></center> <p><b>Ernst Wilhelm</b> \"<b>Wim</b>\" <b>Wenders</b> is a German filmmaker, playwright, author, and photographer. He is a major figure in New German Cinema. Among the honors he has received are prizes from the Cannes, Venice and Berlin film festivals. He has also received a BAFTA Award and been nominated for three Academy Awards and a Grammy Award. <i class=\"fab fa-wikipedia-w\"></i></p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "Iv8YO5BXCAQ"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/perfect_days/",
-	"rymRating": "3.81",
-	"rymCoverartImg": "https://e.snmc.io/i/300/w/87fb70cc5f46ff0e6bdf39d86fec66dd/10982891",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"ES",
-		"CL",
-		"UY",
 		"US"
 	],
-	"title": "Society of the Snow",
-	"originalTitle": "La sociedad de la nieve",
-	"titleLocal": "\u015anie\u017cne bractwo",
-	"year": 2024,
-	"url": "https://www.imdb.com/title/tt16277242",
-	"imdbId": "tt16277242",
-	"imdbRating": 7.8,
+	"title": "Minority Report",
+	"originalTitle": "",
+	"titleLocal": "Raport mniejszo\u015bci",
+	"year": 2002,
+	"url": "https://www.imdb.com/title/tt0181689",
+	"imdbId": "tt0181689",
+	"imdbRating": 7.6,
 	"directors": [
-		"J.A. Bayona"
+		"Steven Spielberg"
 	],
-	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Jurassic_World_Fallen_Kingdom_Japan_Premiere_Red_Carpet_J._A._Bayona_%28cropped%29_%28cropped%29.jpg/320px-Jurassic_World_Fallen_Kingdom_Japan_Premiere_Red_Carpet_J._A._Bayona_%28cropped%29_%28cropped%29.jpg\" class=\"popup-director-photo\"></center> <p><b>Juan Antonio Garc\u00eda Bayona</b> is a Spanish filmmaker. <i class=\"fab fa-wikipedia-w\"></i></p>",
+	"popup-director": "<center><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/MKr25425_Steven_Spielberg_%28Berlinale_2023%29.jpg/320px-MKr25425_Steven_Spielberg_%28Berlinale_2023%29.jpg\" class=\"popup-director-photo\"></center> <p><b>Steven Allan Spielberg</b> is an American film director, producer and screenwriter. A major figure of the New Hollywood era and pioneer of the modern blockbuster, he is the most commercially successful director in history. He is the recipient of many accolades, including three Academy Awards, two BAFTA Awards, and four Directors Guild of America Awards, as well as the AFI Life Achievement Award in 1995, the Kennedy Center Honor in 2006, the Cecil B. DeMille Award in 2009 and the Presidential Medal of Freedom in 2015. Seven of his films have been inducted into the National Film Registry by the Library of Congress as \"culturally, historically or aesthetically significant\". <i class=\"fab fa-wikipedia-w\"></i></p>",
 	"isDocu": false,
 	"yttrailer": {
-		"videoId": "pDak4qLyF4Q"
+		"videoId": "lG7DGMgfOb8"
 	},
-	"rymUrl": "https://rateyourmusic.com/film/la_sociedad_de_la_nieve/",
-	"rymRating": "3.44",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/1588c7b429ac412a10bcd727a0f20fd0/11275710",
-	"rtUrl": "",
-	"rtRating": ""
-},
-{
-	"flag": [
-		"DE"
-	],
-	"title": "The Teachers' Lounge",
-	"originalTitle": "Das Lehrerzimmer",
-	"titleLocal": "Pok\u00f3j nauczycielski",
-	"year": 2023,
-	"url": "https://www.imdb.com/title/tt26612950",
-	"imdbId": "tt26612950",
-	"imdbRating": 7.5,
-	"directors": [
-		"Ilker \u00c7atak"
-	],
-	"popup-director": "<p><b>İlker Çatak</b> is a German writer and director. He won the Student Academy Award in Gold in 2015 with his graduation film <i>Fidelity</i>. His filmography includes works such as <i>Once Upon a Time... Indianerland</i>, <span><i>I was, I am, I will be</i></span> and <span><i>The Teachers' Lounge</i></span> which received numerous awards including an Academy Award nomination for Best International Film and established him as a versatile and innovative filmmaker.</p>",
-	"isDocu": false,
-	"yttrailer": {
-		"videoId": "6YgQBGqhTcM"
-	},
-	"rymUrl": "https://rateyourmusic.com/film/das_lehrerzimmer/",
-	"rymRating": "3.51",
-	"rymCoverartImg": "https://e.snmc.io/i/300/s/8af65e1f929c7461a7e14ca030f54b42/10702077",
+	"rymUrl": "https://rateyourmusic.com/film/minority_report/",
+	"rymRating": "3.27",
+	"rymCoverartImg": "https://e.snmc.io/i/300/s/c9f103aa032096c0472761f907f3d0ce/4367371",
 	"rtUrl": "",
 	"rtRating": ""
 }
+
 
 ]
