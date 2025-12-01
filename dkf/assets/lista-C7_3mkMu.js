2940be049b5dcdcfa605e9e9dbe73d31aa1b1cf4
antek <antek@antek.com>
2025-12-01 10:01:46 +0100

    pages update

---
+++ dkf/assets/lista-C7_3mkMu.js
@@ -0,0 +1,1 @@
+import"./prefix__plugin-vue_export-helper-DARtnqvW.js";import"./prefix_all-BrmZXyuL.js";import{F as l}from"./prefix_flags-5ZjWCBNQ.js";for(let t of document.querySelectorAll(".smallflag")){let e=t.getAttribute("cc");e!==null&&t.setAttribute("src",l[e])}
